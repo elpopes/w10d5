a0f350c885781002a40c7cab9776c920276eab67
rexqu <junjiequ123@gmail.com>
2023-01-20 17:25:14 -0500

    worked on project

--- art-museum/src/components/GalleryNavigation/index.js
+++ art-museum/src/components/GalleryNavigation/index.js
@@ -1,7 +1,27 @@
 import { NavLink } from "react-router-dom";
 
+// function GalleryNavigation({ records }) {
+//   return <nav>
+//     records.map(ele => ({<NavLink prop={ele} />}))
+//     </nav>;
+// }
 function GalleryNavigation({ records }) {
-  return <nav>{records.map(ele => ({<NavLink prop={ele} />}))}</nav>;
+  return (
+    <nav>
+      <NavLink to={"/"}>
+        HOME <br></br>
+      </NavLink>
+
+      {records.map(({ id, name }) => (
+        <NavLink key={id} to={`/galleries/${id}`}>
+          {name}
+          <br></br>
+        </NavLink>
+      ))}
+    </nav>
+  );
 }
 
 export default GalleryNavigation;
+
+
